dea65f024f9dd2acb3dde9c7bb2df5d75a503f42
Observer <80362048+68756d616e@users.noreply.github.com>
2022-10-21 19:10:54 +0100

    Add files via upload

---
+++ #PEMDA Function.py
@@ -0,0 +1,17 @@
+#PEMDA Function
+#Its order of operation
+# Parenthesis () FIRST
+# Exponents n above a (^)
+# Multiplication or Division ; Left to right
+# Addition or Subtraction ; Left to right
+
+# Example 3 * 6 = 18, 9 / 3 = 3, 18 + 3 = 21, 21 - 6 = 15 ; Answer is 15
+print(3 * 6 + 9 / 3 - 6)
+
+# Example 6 + 9 = 15, 3 * 15 = 45, 45 / 3 = 15, 15 - 6 = 9
+print(3 * (6 + 9) / 3 - 6)
+
+#To make the answer and int ; a single number
+print(int(3 * 6 + 9 / 3 - 6))
+
+
